c8e7d08fd8694c331655e113aafa5e3bde20756d
Hasanul Isyraf <hasanul2307@gmail.com>
2019-05-31 16:15:09 +0800

    save image icon

---
+++ senseios/buttonAdditional.swift
@@ -0,0 +1,21 @@
+//
+//  buttonAdditional.swift
+//  senseios
+//
+//  Created by Hasanul Isyraf on 27/05/2019.
+//  Copyright © 2019 Hasanul Isyraf. All rights reserved.
+//
+
+import UIKit
+
+class buttonAdditional: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
